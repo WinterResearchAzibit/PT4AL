93656a1a18e78d0a203f813cada28f415300ec56
azibit <abdulazeezidris28@gmail.com>
2023-01-25 08:45:42 -0600

    Add comments for Config.py file

--- Config.py
+++ Config.py
@@ -1,5 +1,5 @@
-batch_size = 256
-no_of_workers = 1
+batch_size = 256 # Depending on dataset used, batch size changes
+no_of_workers = 1 # For reproducibility, used 1 worker
 
-downstream_epochs = 200
-pretraining_epochs = 15
+downstream_epochs = 200 # Number of epochs for downstream classification
+pretraining_epochs = 15 # Number of epochs for pretraining as in PT4AL
